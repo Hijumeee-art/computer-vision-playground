27bc7ad7e615382825a8e983b4361a0d7d437cd5
Hijumeee-art <kipasterbang657683@gmail.com>
2026-06-10 14:42:20 +0800

    Update hello_ai.py

--- hello_ai.py
+++ hello_ai.py
@@ -1,1 +1,3 @@
-print"(Welcome To Computer Vision Playground)
+print'Welcome to Computer Vision Playground!")
+print ("This repository is used for learning GitHub workflow.")
+print("Al and Computer Vision are exciting technologies!")
